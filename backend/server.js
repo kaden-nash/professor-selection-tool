48fd2a3620c60872a8dc2e4158636a3edc9021ad
simone-c18 <simone.chrastek5@gmail.com>
2026-03-28 16:52:28 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
@@ -12,10 +13,10 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 
 // serve react frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../Frontend/dist", "index.html"));
 });
 
 // test route
