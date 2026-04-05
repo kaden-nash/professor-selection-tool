d0f51574bcea1e71871e9e26613a76bd86143f32
simone-c18 <simone.chrastek5@gmail.com>
2026-04-05 19:55:11 -0400

    removed print from server.js

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 
-console.log(process.env.MONGO_URI);
-
 // connects to mongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
