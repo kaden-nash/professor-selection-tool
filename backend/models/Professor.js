ec358ea43092fde9942dda56bf1957ce9ab2ca89
nyghts <137028304+nyghts7@users.noreply.github.com>
2026-04-05 13:18:10 -0400

    Update Professor.js
    added "department", "numRatings", "avgDifficulty", "avgRating", "wouldTakeAgainPercent", "allReviewsScraped", "fieldOfStudy", "dateJoinedUcf", "graduatedFrom", and "isEmeritus"
    pseudo-embedded the reviews table
    renamed "title" to "role" and added "Associate Lecturer" to the enumeration
    renamed "educationLevel" to "levelOfEducation"
    renamed "coursesTaught" to "courses_taught" and removed "semestersTaught"
    removed "linkedinUrl"
    remastered "scores"

--- backend/models/Professor.js
+++ backend/models/Professor.js
@@ -4,24 +4,49 @@ const mongoose = require("mongoose");
 const professorSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName:  { type: String, required: true },
-  title: {
+  department: { type: String, required: true },
+  numRatings: { type: Number, default: 0 },
+  avgDifficulty: { type: Number, default: 0 },
+  avgRating: { type: Number, default: 0 },
+  wouldTakeAgainPercent: { type: Number, default: 0 },
+  allReviewsScraped: { type: Boolean, default: false },
+  reviews: [
+	{
+		attendanceMandatory: { type: Boolean, default: null },
+		clarityRating: { type: Number, default: 0 },
+		class: { type: String, default: "unknown" },
+		comment: { type: String, default: "No Comments" },
+		date: { type: Date, default: null},
+		difficultyRating: { type: Number, default: 0 },
+		grade: { type: String, enum: ["A", "B", "C", "D", "E", "F", "S", "U"] },
+		helpfulRating: { type: Number, default: 0 },
+		isForCredit: { type: Boolean, default: null },
+		isForOnlineClass: { type: Boolean, default: null },
+		ratingTags: [{ type: String }],
+		teacherNote: { type: Boolean, default: null },
+		textbookUse: { type: Number, default: 0 },
+		thumbs: [],
+		thumbsDownTotal: { type: Number, default: 0 },
+		thumbsUpTotal: { type: Number, default: 0 },
+		wouldTakeAgain: { type: Boolean, default: null }
+	}
+  ],
+  role: {
     type: String,
-    enum: ["Lecturer", "Research Professor", "Adjunct"]
+    enum: ["Lecturer", "Research Professor", "Associate Lecturer", "Adjunct"]
   },
-  educationLevel: { type: String },
-  linkedinUrl:    { type: String },
-  calculatedScores: {
-    composite:    { type: Number, default: 0 },
-    quality:      { type: Number, default: 0 },
-    reliability:  { type: Number, default: 0 },
-    englishSkill: { type: Number, default: 0 }
+  fieldOfStudy: { type: String },
+  dateJoinedUcf: { type: Date, default: null },
+  levelOfEducation: { type: String, default: "unknown" },
+  graduatedFrom: { type: String, default: "unknown" },
+  isEmeritus: {type: Boolean, default: false },
+  courses_taught: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
+  scores: {
+    difficultyScore: { type: Number, default: 0 },
+	qualityScore: { type: Number, default: 0 },
+	wouldTakeAgainPercent: { type: Number, default: 0 },
+	archetype: { type: String, enum: ["The Unicorn", "The Mastermind", "The NPC", "The Saboteur"] }
   },
-  coursesTaught: [
-    {
-      courseId:        { type: mongoose.Schema.Types.ObjectId, ref: "Course" },
-      semestersTaught: { type: Number, default: 0 }
-    }
-  ]
 }, { timestamps: true });
 
 module.exports = mongoose.model("Professor", professorSchema);
